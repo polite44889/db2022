93979279c4f940931fd317eb50cc82c2e423649f
polite44889 <47509386+polite44889@users.noreply.github.com>
2022-02-07 04:20:42 +0200

    Create Warehouse(fruits)1.sql

---
+++ lab1/Warehouse(fruits)1.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "Fruit" (
+  "id" integer PRIMARY KEY,
+  "weight" float,
+  "country" text,
+  "storage" integer
+);
+
+CREATE TABLE "Categories" (
+  "id_categories" integer PRIMARY KEY,
+  "type" text,
+  "categories" varchar
+);
+
+CREATE TABLE "Country" (
+  "id" integer PRIMARY KEY,
+  "country" text,
+  "manufacturer" text,
+  "purchase_price" float,
+  "categories" varchar
+);
+
+CREATE TABLE "Storage" (
+  "id" integer PRIMARY KEY,
+  "storage" integer,
+  "address" text,
+  "date_in" date
+);
+
+CREATE TABLE "Price" (
+  "id" integer PRIMARY KEY,
+  "price" integer,
+  "fruit" text
+);
+
+ALTER TABLE "Storage" ADD FOREIGN KEY ("storage") REFERENCES "Fruit" ("storage");
+
+ALTER TABLE "Country" ADD FOREIGN KEY ("country") REFERENCES "Fruit" ("country");
+
+ALTER TABLE "Categories" ADD FOREIGN KEY ("id_categories") REFERENCES "Country" ("categories");
+
+ALTER TABLE "Price" ADD FOREIGN KEY ("id") REFERENCES "Country" ("purchase_price");
